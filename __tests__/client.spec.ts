75853d7b73e6e676f3c281075d9c263d7fac4408
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2023-12-04 03:00:36 +0000

    test: add cases

--- __tests__/client.spec.ts
+++ __tests__/client.spec.ts
@@ -1,17 +1,21 @@
-import { createClient } from '@src/client.js'
+import { createBatchClient, createClient } from '@src/client.js'
 import WebSocket, { WebSocketServer } from 'ws'
 import { createServer } from '@src/server.js'
 import { waitForEventEmitter } from '@blackglory/wait-for'
 import { getErrorPromise } from 'return-style'
 import { promisify } from 'extra-promise'
+import { createBatchProxy } from 'delight-rpc'
 
 interface IAPI {
   echo(message: string): string
   error(message: string): never
 }
 
+const SERVER_URL = 'ws://localhost:8080'
+
 let server: WebSocketServer
-beforeEach(() => {
+let wsClient: WebSocket
+beforeEach(async () => {
   server = new WebSocketServer({ port: 8080 })
   server.on('connection', socket => {
     const cancelServer = createServer<IAPI>({
@@ -23,38 +27,53 @@ beforeEach(() => {
       }
     }, socket)
   })
+
+  wsClient = new WebSocket(SERVER_URL)
+  await waitForEventEmitter(wsClient, 'open')
 })
 afterEach(async () => {
+  wsClient.close()
+
   await promisify(server.close.bind(server))()
 })
 
 describe('createClient', () => {
   test('echo', async () => {
-    const wsClient = new WebSocket('ws://localhost:8080')
-    await waitForEventEmitter(wsClient, 'open')
+    const [client] = createClient<IAPI>(wsClient)
+    const result = await client.echo('hello')
 
-    try {
-      const [client] = createClient<IAPI>(wsClient)
-      const result = await client.echo('hello')
+    expect(result).toBe('hello')
+  })
 
-      expect(result).toBe('hello')
-    } finally {
-      wsClient.close()
-    }
+  test('echo (batch)', async () => {
+    const [client, close] = createBatchClient(wsClient)
+    const proxy = createBatchProxy<IAPI>()
+
+    const result = await client.parallel(proxy.echo('hello'))
+    close()
+
+    expect(result.length).toBe(1)
+    expect(result[0].unwrap()).toBe('hello')
   })
 
   test('error', async () => {
-    const wsClient = new WebSocket('ws://localhost:8080')
-    await waitForEventEmitter(wsClient, 'open')
+    const [client] = createClient<IAPI>(wsClient)
+    const err = await getErrorPromise(client.error('hello'))
 
-    try {
-      const [client] = createClient<IAPI>(wsClient)
-      const err = await getErrorPromise(client.error('hello'))
+    expect(err).toBeInstanceOf(Error)
+    expect(err!.message).toMatch('hello')
+  })
 
-      expect(err).toBeInstanceOf(Error)
-      expect(err!.message).toMatch('hello')
-    } finally {
-      wsClient.close()
-    }
+  test('error (batch)', async () => {
+    const [client, close] = createBatchClient(wsClient)
+    const proxy = createBatchProxy<IAPI>()
+
+    const result = await client.parallel(proxy.error('hello'))
+    close()
+
+    expect(result.length).toBe(1)
+    const err = result[0].unwrapErr()
+    expect(err).toBeInstanceOf(Error)
+    expect(err!.message).toMatch('hello')
   })
 })
